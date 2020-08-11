61db9648afc4d8e68f7df842fc93101b5dc8c286
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-08-12 01:32:17 +0200

    log domain

---
+++ public/index.js
@@ -0,0 +1,13 @@
+const domain = location.origin
+console.log("domain", domain)
+
+
+// const ws = new WebSocket('ws://www.host.com/path');
+ 
+// ws.on('open', function open() {
+//   ws.send('something');
+// });
+ 
+// ws.on('message', function incoming(data) {
+//   console.log(data);
+// });
