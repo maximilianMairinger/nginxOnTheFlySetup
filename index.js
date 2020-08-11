d91033da04c83b9e336a5f31fcbb699a1201a565
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-08-12 01:16:20 +0200

    sample server

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log()
+const express = require("express")
+const bodyParser = require("body-parser")
+const app = express()
+const args = require("yargs").argv
+const port = args.port !== undefined ? args.port : console.log("Serving on port 443\n") || 443
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+
+
+app.get("/", (req, res) => {
+  res.send(req.subdomains)
+})
+
+
+
+
+
+app.listen(port, () => {console.log("Started on port", port)})
