8a0652abe2b7c32a9d804474bdaeaa9d227ebbfe
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-08-12 01:49:45 +0200

    fix?

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     script: "server.js",
     name: "nginxOnTheFlySetup",
-    exec_mode : "cluster",
+    // exec_mode : "cluster",
     instances: 2,
     wait_ready: true,
     args: "--port 4400"
