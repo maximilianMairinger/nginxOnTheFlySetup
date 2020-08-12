b7bbe9a8f140f9e79b76d9534af926ca89727100
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-08-13 00:26:47 +0200

    log, inq gui

--- public/index.js
+++ public/index.js
@@ -1,9 +1,8 @@
-const domain = location.origin
-console.log("domain", domain)
+let subdomains = location.host.split(".").reverse()
+subdomains.splice(0, 3)
 
 
-
-const ws = new WebSocket("ws://qwer.qq.maximilian.mairinger.com/ws/");
+const ws = new WebSocket("ws://" + location.host + "/ws/");
  
 ws.addEventListener('open', function open() {
   console.log("open")
@@ -13,3 +12,40 @@ ws.addEventListener('open', function open() {
 ws.addEventListener('message', function incoming(data) {
   console.log(data);
 });
+
+
+
+const { log, inq } = (() => {
+  function apd(q) {
+    body.insertAdjacentHTML("beforeend", q)
+  }
+  const body = document.querySelector("div.body")
+  function log(msg) {
+    apd(`<div class="message">${msg}</div><br>`)
+  }
+
+  let id = 0
+  function inq(question, type = "text", nonoPostFix = false) {
+    return new Promise((res) => {
+      if (!(question.endsWith("?") || question.endsWith(":")) && !nonoPostFix) question = question + ":"
+      apd(`<div class="message">${question}</div><input id="inp${id}" type="${type}" autocomplete="off"><br>`)
+      let inputElem = document.getElementById("inp" + id)
+      inputElem.focus()
+      inputElem.addEventListener("keydown", ({key}) => {
+        if (key === "Enter") {
+          inputElem.setAttribute("disabled", true)
+          res(inputElem.value)
+        }
+      })
+      id++
+    })
+    
+  }
+
+  return { log, inq }
+})()
+
+setTimeout(() => {
+  log("Hello")
+  inq("What")
+}, 1000)
