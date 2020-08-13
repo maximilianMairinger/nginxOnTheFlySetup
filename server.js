06767e3aad55e553288a5e9aedd1143c466fe6e0
Maximilian Mairinger <maximilian.mairinger@gmx.at>
2020-08-13 22:02:07 +0200

    try call fully implemented

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const shell = require("shelljs")
 require("xrray")()
 require("xtring")()
 const slugify = require("slugify")
+const path = require("path")
+const { promises: fs } = require("fs")
+const detectPort = require("detect-port")
+const sanitizeFilename = require("sanitize-filename")
+
+// config
+const appDest = "/var/www/html"
+const nginxDest = "/etc/nginx"
+const githubUsername = "maximilianMairinger"
+const startPort = 5000
+
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
@@ -23,58 +35,102 @@ wss.on('connection', (ws) => {
 app.use(express.static('public'))
 
 
-app.post("/try", ({body: q}, res) => {
-  let r = shell.cd("/mnt/c/Users/Maximilian Mairinger/Desktop/test/")
-  if (r.code !== 0) {
-    res({err: "UnexpectedError"})
+
+/*
+{
+  id: number,
+  commit: {
+    repo: string,
+    hash: string
+  },
+  domain?: string
+}
+*/
+app.post("/try", async ({body: q}, res) => {
+  try {
+
+    let old = q.commit.repo
+    q.commit.repo = sanitizeFilename(old)
+    if (old !== q.commit.repo) {
+      res({err: "Go away! :c"})
+      return
+    }
+    old = undefined
+
+    let repoPath = path.join(appDest, q.commit.repo)
+    try {
+      await fs.access(repoPath)
+    }
+    catch(e) {
+      res({err: `${q.commit.repo} is not an active repository.`})
+      return
+    }
+
+
+    if (!q.domain) q.domain = q.commit.hash + "." + q.commit.repo
+
+
+    if (!q.domain.endsWidth(".maximilian.mairinger.com")) q.domain = q.domain + ".maximilian.mairinger.com"
+    q.domain = slugify(q.domain.toLowerCase())
+
+
+    
+    let ws = wsLs[q.id]
+
+
+
+
+    let createAppConf
+    let createNginxConf
+    try {
+      createAppConf = require("./../nginxCdSetup/app/createAppConf.js")
+      createNginxConf = require("./../nginxCdSetup/app/createNginxConf.js")
+    }
+    catch(e) {
+      console.log("Unable to find peer dependency at './../nginxCdSetup/app/createAppConf.js'. Make sure https://github.com/maximilianMairinger/nginxCdSetup is installed in the neighboring folder.")
+      res({err: "Unable to find peer dependencies. Check logs for additional infos."})
+      return
+    }
+
+
+    
+
+    
+    let conf = {appDest, nginxDest, domain: q.domain, name: q.commit.repo, hash: q.commit.hash, port: await detectPort(startPort), githubUsername}
+
+    function updateClient(msg) {
+      ws.send({log: msg})
+    }
+
+    try {
+      await createAppConf(conf, updateClient)
+      await createNginxConf(conf, updateClient)
+      res({log: "Done"})
+      console.log("Done")
+    } catch (e) {
+      res({err: e.message})
+      console.log("Error: " + e.message)
+      console.log("Cmd: " + e.cmd)
+      console.log("Stderr: " + e.stderr)
+    }
+
+    
+    
+    
+
+
+
+
   }
-  else {
-    if (q.commit.repo !== undefined) {
-      r = shell.exec(`git clone https://github.com/maximilianMairinger/${q.commit.repo}`, {silent: true})
-      if (r.code !== 0) {
-        res({err: "RepoNotFound"})
-      }
-      else if (q.commit.hash !== undefined) {
-        r = shell.cd(q.commit.repo)
-        if (r.code !== 0) {
-          res({err: "UnexpectedError"})
-        }
-        else {
-          r = shell.exec(`git checkout ${q.commit.hash}`, {silent: true})
-          if (r.code !== 0) {
-            res({err: "HashNotFound"})
-          }
-          else {
-            r = shell.exec(`git reset --hard`, {silent: true})
-            if (r.code !== 0) {
-              res({err: "UnexpectedError"})
-            }
-            else {
-              res({suc: true})
-              startSetup(q)
-            }
-          }
-        }
-      }
-      else {
-        res({suc: true})
-        startSetup(q)
-      }
-    }
-    else {
-      res()
-    }
+  catch(e) {
+    console.log("Unexpected error in /try call: ", e)
   }
 
   
 })
 
 
-function startSetup(q) {
-  let ws = wsLs[q.id]
-  if (q.name === undefined) q.name = q.commit.repo
-  q.name = slugify(q.name.toLowerCase())
-}
+
 
 
 
